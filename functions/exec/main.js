0420173be096d18756a1a086f37cc2e70eb2d00d
James Portune <jayms8462>
2025-09-15 21:07:30 -0500

    Pulling down prod site since still under construction

--- functions/exec/main.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('hamburger').addEventListener('click', () => {
-  document.querySelector('.nav-links ul').classList.toggle('active');
-});
--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
 const app = require("./app");
 
+// Export the Express app as a Firebase Function (1st Gen)
 exports.app = functions.https.onRequest(app);
